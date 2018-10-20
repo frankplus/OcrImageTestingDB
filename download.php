2654771e23d95e72b3c345b4b79af98a968f147e
Unknown <stefano.romanello@hotmail.it>
2018-10-20 13:30:03 +0200

    Sistemato download che scaricava file corrotto

--- download.php
+++ download.php
@@ -13,7 +13,24 @@ if (!isset( $_SESSION['user'] ) ) {
     }
     $zip->close();
 
-    header('Content-Type: application/zip');
-    header("Content-Disposition: attachment; filename = $download");
-    header('Content-Length: ' . filesize($download));
+    $file=$download;
+    if (headers_sent()) {
+        echo 'HTTP header already sent';
+    } else {
+        if (!is_file($file)) {
+            header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
+            echo 'File not found';
+        } else if (!is_readable($file)) {
+            header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
+            echo 'File not readable';
+        } else {
+            header($_SERVER['SERVER_PROTOCOL'].' 200 OK');
+            header("Content-Type: application/zip");
+            header("Content-Transfer-Encoding: Binary");
+            header("Content-Length: ".filesize($file));
+            header("Content-Disposition: attachment; filename=\"".basename($file)."\"");
+            readfile($file);
+            exit;
+        }
+    }
 ?>
