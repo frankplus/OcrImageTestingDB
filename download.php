c2a71c9d5cc1b7312be6710ea8861602a992a921
Unknown <stefano.romanello@hotmail.it>
2018-10-20 13:24:41 +0200

    Riportato download a file singolo

--- download.php
+++ download.php
@@ -1,142 +1,19 @@
 <?php
 session_start();
 if (!isset( $_SESSION['user'] ) ) {
-    echo '<script>window.location.href = "http://localhost/index.php";</script>';
+  header("location: /index.php");
 }
+
+
+    $zip = new ZipArchive;
+    $download = 'tmp/fotoDb.zip';
+    $zip->open($download, (ZipArchive::CREATE | ZipArchive::OVERWRITE));
+    foreach (glob("foto/*") as $file) {
+        $zip->addFile($file);
+    }
+    $zip->close();
+
+    header('Content-Type: application/zip');
+    header("Content-Disposition: attachment; filename = $download");
+    header('Content-Length: ' . filesize($download));
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Elementi di Ingegneria</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- MetisMenu CSS -->
-    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-</head>
-
-<body>
-
-    <div id="wrapper">
-
-        <!-- Navigation -->
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="inserimento.php">Gestione foto - Elementi di Ingegneria</a>
-            </div>
-            <!-- /.navbar-header -->
-
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li>
-                            <a href="inserimento.php"><i class="fa fa-pencil fa-fw"></i> Inserimento dati</a>
-                        </li>
-                        <li>
-                            <a href="visualizza.php"><i class="fa fa-table fa-fw"></i> Visualizza dati</a>
-                        </li>
-                        <li>
-                            <a href="download.php"><i class="fa fa-download fa-fw"></i> Download dati</a>
-                        </li>
-                        <li>
-                            <a href="logout.php"><i class="fa fa-key fa-fw"></i> Logout</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
-        </nav>
-
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Download</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-
-            <div class="row">
-
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Scarica paccheteto foto con metadati
-                    </div>
-                    <div class="panel-body">
-                            <?php
-                                
-                                    $zip = new ZipArchive;
-                                    $download = 'tmp/fotoDb.zip';
-                                    $zip->open($download, (ZipArchive::CREATE | ZipArchive::OVERWRITE));
-                                    foreach (glob("foto/*") as $file) {
-                                        $zip->addFile($file);
-                                    }
-                                    $zip->close();
-
-                                    
-                                    /*header('Content-Type: application/zip');
-                                    header("Content-Disposition: attachment; filename = $download");
-                                    header('Content-Length: ' . filesize($download));*/
-                                    echo '<H2><a href="http://localhost/tmp/fotoDb.zip">Scarica</button></H2>';
-                                
-                            ?>
-                    </div>
-                </div>
-
-
-                
-
-            </div>
-            <!-- /.row -->
-        </div>
-        <!-- /#page-wrapper -->
-
-    </div>
-    <!-- /#wrapper -->
-
-    <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="dist/js/sb-admin-2.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="vendor/raphael/raphael.min.js"></script>
-    <script src="vendor/morrisjs/morris.min.js"></script>
-    <script src="data/morris-data.js"></script>
-
-
-
-
-</body>
-
-</html>
-
